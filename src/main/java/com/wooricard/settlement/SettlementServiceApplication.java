df76f22e829cac8b7a75c551deca7b9d0631bea9
light11014 <dtq1014@naver.com>
2026-03-23 21:27:15 +0900

    docs: add SPRING_BATCH.md

--- src/main/java/com/wooricard/settlement/SettlementServiceApplication.java
+++ src/main/java/com/wooricard/settlement/SettlementServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 카드 정산 애플리케이션
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableScheduling
 public class SettlementServiceApplication {
     public static void main(String[] args) {
         // .env 파일을 읽어서 시스템 프로퍼티로 복사
